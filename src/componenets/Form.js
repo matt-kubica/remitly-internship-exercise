60c6522ba99abd91e7b6d0edbf6ee2d627a26df8
qbic22562 <mateusz.kubica99@gmail.com>
2021-03-07 15:17:24 +0100

    Improved Form component

--- src/componenets/Form.js
+++ src/componenets/Form.js
@@ -1,25 +1,29 @@
-import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-
-const Form = () => {
-  const [firstCurrency, setFirstCurrency] = useState('')
-  const [secondCurrency, setSecondCurrency] = useState('')
+const Form = ({ firstValue, secondValue, setFirstValue, setSecondValue }) => {
 
   return (
     <form className='converter-form'>
       <div className='form-control'>
         <label>You send: </label>
         <input className='gb' type='text' placeholder='GBP' 
-               value={firstCurrency} onChange={(e) => setFirstCurrency(e.target.value)}/>
+               value={firstValue}
+               onChange={(e) => setFirstValue(e.target.value)}/>
       </div>
       <div className='form-control'>
         <label>They receive: </label>
         <input className='pl' type='text' placeholder='PLN'
-               value={secondCurrency} onChange={(e) => setSecondCurrency(e.target.value)}/>
+               value={secondValue}
+               onChange={(e) => setSecondValue(e.target.value)}/>
       </div>
       
     </form>
   )
+}
+
+Form.propTypes = {
+  setFirstValue: PropTypes.func,
+  setSecondValue: PropTypes.func,
 }
 
 export default Form
